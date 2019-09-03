a3788d4f05b37f4e6eaf8728b45a788ff4c0caec
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-09-03 22:21:54 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+class Project
+  attr_accessor :backer
+  attr_reader :title, :backers
+
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer 
+  end
+
+end
